19d8125f781ee6e402d551c2a527f90b8babb7a1
Ernstjan Hoek <ernstjanhoek@tutanota.com>
2024-01-02 23:11:08 +0100

    Onnodig try catch in main

--- Car Sharing/task/src/carsharing/Main.java
+++ Car Sharing/task/src/carsharing/Main.java
@@ -4,16 +4,13 @@ import java.sql.*;
 import java.util.Scanner;
 public class Main {
     static String connectionUrl;
-    public static void main(String[] args) throws SQLException, ClassNotFoundException, InterruptedException {
+    public static void main(String[] args) throws Exception {
         //  database URL
         connectionUrl = "jdbc:h2:./src/carsharing/db/" + args[1];
         Scanner scanner = new Scanner(System.in);
         CarSharingApp menu = new Init();
-        try {
-            menu.processInput();
-            menu = menu.transition();
-        } catch (Exception ignore) {}
-
+        menu.processInput();
+        menu = menu.transition();
         while (menu.running) {
             try {
                 System.out.println(menu.displayMessage());
